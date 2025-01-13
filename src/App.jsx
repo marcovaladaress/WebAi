70605611b7da11a0fcbe194b36f9c7b5ec78885f
marco valadares <marcovf99@gmail.com>
2025-01-13 19:32:15 -0300

    criando cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import ArrowUp from "./components/arrow-up";
 import Headertopo from "./components/header";
+import SectionMain from "./components/section-main";
 
 
 
@@ -9,32 +9,270 @@ export default function App() {
   return (
     <>
       <Headertopo />
+      <SectionMain />
       <section>
-        <div className="container">
-          <div className="section-container">
-          <div className="container-info">
-            <div className="infos">
-                <span><p className="collect-info">Collect Nfts</p></span>
-                <h1><span className="title">Discover & Collect</span> The Best NFTs Digital Art</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora veritatis consectetur esse cum neque voluptatibus.</p>
-                <button className="explore-info">Explore Now</button>
-               <span> <button className="sell-info">Sell Now <ArrowUp/> </button></span>
-            </div>
-          </div>
-          <div className="container-img">
-                <div className="img1">
-                    <img width="300px" className="img-monkey" src="./img-monkey1.png" alt="" />
-                    <img width="300px" className="img-monkey" src="./img-monkey2.png" alt="" />
-                </div>
-                <div className="img2">
-                    <img width="300px" className="img-monkey" src="./img-monkey3.png" alt="" />
-                    <img width="300px" className="img-monkey" src="./img-monkey4.png" alt="" />
-                </div>
-          </div>
+        <div className="info-contegories">
+          <h2>Trending Categories</h2>
+          <p>Lorem ipsum dolor  repellendus mafcvsjqfjqjfkqcbdnqbvcq.m  temporiburunt similique ducimus mecati quisquam ad impedit! Voluptatum!</p>
         </div>
+        <div className="container-cards">
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./card1.png" alt="" />
+              <div className="users">
+                <img src="./users-1.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Nikom</a></span>
+                  <span><p>Half Hair</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
           </div>
 
-          
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./img-monkey3.png" alt="" />
+              <div className="users">
+                <img src="./users-2.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Mexi</a></span>
+                  <span><p>Headphone</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./img-monkey4.png" alt="" />
+              <div className="users">
+                <img src="./users-3.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Astro</a></span>
+                  <span><p>Goggles</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./card-2.png" alt="" />
+              <div className="users">
+                <img src="./users-4.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Aliza</a></span>
+                  <span><p>Cute Girl</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./card-3.png" alt="" />
+              <div className="users">
+                <img src="./users-5.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Tony</a></span>
+                  <span><p>Sky Hair Girl</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./card4.png" alt="" />
+              <div className="users">
+                <img src="./userss-6.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">Alabto</a></span>
+                  <span><p>Cartoon Couple</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./card5.png" alt="" />
+              <div className="users">
+                <img src="./users-7.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Minato</a></span>
+                  <span><p>Skeleton</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+          <div className="cards">
+            <div className="car1 card ">
+              <img src="./img-monkey1.png" alt="" />
+              <div className="users">
+                <img src="./users-8.png" alt="" />
+                <div className="users-information ">
+                  <span><a href="#">@Tamari</a></span>
+                  <span><p>Bussinessman</p></span>
+                </div>
+                <div className="love-card">
+                  <a href="#"><img src="./heart.png" alt="" /></a>
+                </div>
+              </div>
+
+              <div className="etherium-card">
+                <span>Current Bid</span>
+                <span>Last Bid</span>
+              </div>
+
+              <div className="icon-etherium-card">
+                <span><img src="./coins.png" alt="" /> 20.05 ETH</span>
+                <span><img src="./coins.png" alt="" /> 25.05 ETH</span>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+
+        </div>
+
       </section>
     </>
 
---
+++ src/components/section-main.jsx
@@ -0,0 +1,34 @@
+import ArrowUp from "./arrow-up";
+
+export default function SectionMain() {
+
+    return(
+        <section>
+                <div className="container">
+                  <div className="section-container">
+                  <div className="container-info">
+                    <div className="infos">
+                        <h1><span className="title">Discover & Collect</span> The Best NFTs Digital Art</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora veritatis consectetur esse cum neque voluptatibus.</p>
+                        <button className="explore-info">Explore Now</button>
+                       <span> <button className="sell-info">Sell Now <ArrowUp/> </button></span>
+                    </div>
+                  </div>
+                  <div className="container-img">
+                        <div className="img1">
+                            <img width="300px" className="img-monkey" src="./img-monkey1.png" alt="" />
+                            <img width="300px" className="img-monkey" src="./img-monkey2.png" alt="" />
+                        </div>
+                        <div className="img2">
+                            <img width="300px" className="img-monkey" src="./img-monkey3.png" alt="" />
+                            <img width="300px" className="img-monkey" src="./img-monkey4.png" alt="" />
+                        </div>
+                  </div>
+                </div>
+                  </div>
+        
+                 
+                  
+              </section>
+    )
+}
